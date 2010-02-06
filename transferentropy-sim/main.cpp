c064bd272f194307c14187ad3467b292442e080d
Olav Stetter <olav.stetter@googlemail.com>
2010-02-06 01:54:17 +0000

    TE-S hinzugefügt, ein paar Probleme mit der Diskretisierung verbleiben - so ist das Ergebnis immer noch nicht so gut wir vom ursprünglichen xresponse-15bins-Modell

---
+++ transferentropy-sim/main.cpp
@@ -0,0 +1,427 @@
+// calculate the transfer entropy between a numer of time series
+// created by olav, Mi 10 Jun 2009 19:42:11 IDT
+
+#include <cstdlib>
+#include <iostream>
+#include <cassert>
+#include <fstream>
+#include <ctime>
+#include <cstring>
+#include <sstream>
+#include <gsl/gsl_rng.h>
+#include <gsl/gsl_randist.h>
+
+#include "../../Simulationen/olav.h"
+#include "../../Simulationen/SimKernel/sim_main.h"
+
+#ifndef INLINE
+#define INLINE extern inline
+#endif
+
+#define REPORTS 25
+#undef SHOW_DETAILED_PROGRESS
+
+#define RESULT_DIMENSION 1
+
+using namespace std;
+
+typedef unsigned char rawdata;
+
+
+class Kernel;
+
+int main(int argc, char* argv[])
+{
+   SimControl<Kernel> simc;
+   int ret = simc.simulate(argc, argv);
+   return 0;
+};
+
+class Kernel
+{
+public:
+	unsigned long iteration;
+	unsigned int size;
+	unsigned int bins;
+	unsigned int rawdatabins;
+	// unsigned long mag der Kernel irgendwie nicht?
+	long samples;
+	unsigned int word_length;
+	double std_noise;
+	string inputfile_name;
+	string outputfile_results_name;
+	string outputfile_pars_name;
+	gsl_rng* GSLrandom;
+	double input_scaling;
+
+	unsigned long* F_Ipast;
+	unsigned long** F_Inow_Ipast;
+	unsigned long** F_Ipast_Jpast;
+	unsigned long*** F_Inow_Ipast_Jpast;
+  rawdata **xdata;
+  double **xresult;
+
+  void initialize(Sim& sim)
+	{
+		iteration = sim.iteration();
+		sim.io <<"Init: iteration "<<iteration<<", process "<< sim.process()<<Endl;
+		
+		// read parameters from control file
+		sim.get("size",size);
+		sim.get("rawdatabins",rawdatabins);
+		sim.get("bins",bins);
+		sim.get("samples",samples);
+		sim.get("p",word_length);
+		assert(word_length == 1);
+		sim.get("noise",std_noise);
+		sim.get("appliedscaling",input_scaling);
+		
+		sim.get("inputfile",inputfile_name);
+		sim.get("outputfile",outputfile_results_name);
+		sim.get("outputparsfile",outputfile_pars_name);
+
+		// initialize random number generator
+		gsl_rng_env_setup();
+		GSLrandom = gsl_rng_alloc(gsl_rng_default);
+		gsl_rng_set(GSLrandom, 1234);
+	};
+
+	void execute(Sim& sim)
+	{
+	  cout <<"------ transferentropy-sim:main ------ olav, Wed 10 Jun 2009 ------"<<endl;
+	  time_t start, end;
+#ifdef SHOW_DETAILED_PROGRESS
+	  time_t middle;
+#endif
+	  unsigned long totaltrials, completedtrials;
+
+	  time(&start);
+	  cout <<"start: "<<ctime(&start)<<flush;
+	  cout <<"running on host: "<<flush;
+	  system("hostname");
+	  cout <<"current directory: "<<flush;
+	  system("pwd");
+
+	  cout <<"input file: "<<inputfile_name<<endl;
+	  cout <<"output file: "<<outputfile_results_name<<endl;
+
+	  cout <<"allocating memory..."<<flush;
+	  xdata = new rawdata*[size];
+	  xresult = new double*[size];
+	  for(int i=0; i<size; i++)
+	  {
+	    xdata[i] = new rawdata[samples];
+	    memset(xdata[i], 0, samples*sizeof(rawdata));
+	    xresult[i] = new double[size];
+	    memset(xresult[i], 0, size*sizeof(double));
+	  }
+		F_Ipast = new unsigned long[bins];
+		F_Inow_Ipast = new unsigned long*[bins];
+		F_Ipast_Jpast = new unsigned long*[bins];
+		F_Inow_Ipast_Jpast = new unsigned long**[bins];
+		for (rawdata x=0; x<bins; x++)
+		{
+			F_Inow_Ipast[x] = new unsigned long[bins];
+			F_Ipast_Jpast[x] = new unsigned long[bins];
+
+			F_Inow_Ipast_Jpast[x] = new unsigned long*[bins];
+			for (rawdata x2=0; x2<bins; x2++)
+			{
+				F_Inow_Ipast_Jpast[x][x2] = new unsigned long[bins];
+			}
+		}
+	  cout <<" done."<<endl;
+
+	  cout <<"loading data and adding noise (std "<<std_noise<<")... "<<flush;
+	  load_data();
+	  cout <<" done."<<endl;
+
+	  // main loop:
+	  totaltrials = size*(size-1);
+	  cout <<"set-up: "<<size<<" neurons, "<<samples<<" samples, "<<bins<<" bins"<<endl;
+		cout <<"assumed length of Markov chain: "<<word_length<<endl;
+	  completedtrials = 0;
+		// unsigned long long terms_sum = 0;
+		// unsigned long long terms_zero = 0;
+	
+#ifndef SHOW_DETAILED_PROGRESS
+	  	cout <<"running "<<flush;
+#endif
+
+	  for(int ii=0; ii<size; ii++)
+	  {
+#ifndef SHOW_DETAILED_PROGRESS
+	  	status(ii,REPORTS,size);
+#endif
+	    for(int jj=0; jj<size; jj++)
+	    {
+	      if (ii != jj)
+	      {
+#ifdef SHOW_DETAILED_PROGRESS
+	      	cout <<"#"<<ii+1<<" -> #"<<jj+1<<": "<<flush;
+#endif
+	      	xresult[ii][jj] = TransferEntropy(xdata[ii], xdata[jj]);
+					completedtrials++;
+#ifdef SHOW_DETAILED_PROGRESS
+					time(&middle);
+					cout <<" (elapsed "<<sec2string(difftime(middle,start))<<",";
+					// cout <<" ETA "<<sec2string((totaltrials-(completedtrials+1))*difftime(middle,start)/(completedtrials+1))<<")"<<endl;
+					cout <<" ETA "<<ETAstring(completedtrials,totaltrials,difftime(middle,start))<<")"<<endl;
+					// cout <<" (elapsed: "<<sec2string(difftime(middle,start))<<")"<<endl;
+#endif
+	      }
+	      else
+	      {
+#ifdef SHOW_DETAILED_PROGRESS
+					cout <<"#"<<ii+1<<" -> #"<<jj+1<<": skipped."<<endl;
+#endif
+	      	xresult[ii][jj] = 0.0;
+	      }
+	    }
+	  }
+#ifndef SHOW_DETAILED_PROGRESS
+	  cout <<" done."<<endl;
+#endif
+
+	  time(&end);
+	  cout <<"end: "<<ctime(&end)<<flush;
+	  cout <<"runtime: "<<sec2string(difftime(end,start))<<endl;
+
+		// cout <<"TE terms: "<<terms_sum<<", of those zero: "<<terms_zero<<" ("<<int(double(terms_zero)*100/terms_sum)<<"%)"<<endl;
+	};
+	
+	void finalize(Sim& sim)
+	{
+	  write_result(xresult);
+		save_parameters();
+
+		gsl_rng_free(GSLrandom);
+				
+		sim.io <<"End of Kernel (iteration="<<(sim.iteration())<<")"<<Endl;
+	};
+	
+
+	double TransferEntropy(rawdata *arrayI, rawdata *arrayJ)
+	{
+		/* see for reference:
+		     Gourevitch und Eggermont. Evaluating Information Transfer Between Auditory
+		     Cortical Neurons. Journal of Neurophysiology (2007) vol. 97 (3) pp. 2533 */
+	  double result = 0.0;
+
+		// We are looking at the information flow of array1 ("I") -> array2 ("J")
+	
+		// allocate memory
+		memset(F_Ipast, 0, bins*sizeof(unsigned long));
+		for (rawdata x=0; x<bins; x++)
+		{
+			memset(F_Inow_Ipast[x], 0, bins*sizeof(unsigned long));
+			memset(F_Ipast_Jpast[x], 0, bins*sizeof(unsigned long));
+			for (rawdata x2=0; x2<bins; x2++)
+				memset(F_Inow_Ipast_Jpast[x][x2], 0, bins*sizeof(unsigned long));
+		}
+	
+	  // extract probabilities (actually number of occurrence)
+		for (unsigned long t=word_length; t<samples; t++)
+	  {
+			F_Ipast[arrayI[t-1]]++;
+			F_Inow_Ipast[arrayI[t]][arrayI[t-1]]++;
+			F_Ipast_Jpast[arrayI[t-1]][arrayJ[t-1]]++;
+			F_Inow_Ipast_Jpast[arrayI[t]][arrayI[t-1]][arrayJ[t-1]]++;
+#ifdef SHOW_DETAILED_PROGRESS
+			status(t, REPORTS, samples-word_length);
+#endif
+		}
+	
+		for (rawdata k=0; k<bins; k++)
+			for (rawdata l=0; l<bins; l++)
+			{
+				if (F_Ipast_Jpast[k][l] > 0)
+					for (rawdata m=0; m<bins; m++)
+						// if (F_Ipast[m]*F_Inow_Ipast[m][l]*F_Inow_Ipast_Jpast[m][k][l] != 0)
+						if (F_Inow_Ipast_Jpast[m][k][l] != 0)
+							result += F_Inow_Ipast_Jpast[m][k][l]/double(samples-word_length) * \
+								log(double(F_Inow_Ipast_Jpast[m][k][l]*F_Ipast[k])/(F_Ipast_Jpast[k][l]*F_Inow_Ipast[m][k]));
+			}
+
+	  return result/log(2);
+	};
+	
+	void load_data()
+	{
+		char* name = new char[inputfile_name.length()+1];
+		strcpy(name,inputfile_name.c_str());
+	  ifstream binaryfile(name, ios::binary);
+		delete[] name;
+		
+		char* temparray = new char[samples];
+		double xtemp;
+
+	  if (binaryfile == NULL)
+	  {
+	  	cout <<endl<<"error: cannot find input file!"<<endl;
+	  	exit(1);
+	  }
+	
+		// test file length
+		binaryfile.seekg(0,ios::end);
+		if(long(binaryfile.tellg()) != size*samples)
+		{
+	  	cout <<endl<<"error: file length of input does not match given parameters!"<<endl;
+	  	exit(1);
+		}
+		binaryfile.seekg(0,ios::beg);	
+
+	  for(int j=0; j<size; j++)
+	  {
+	    binaryfile.read(temparray, samples);
+	    for(long k=0; k<samples; k++)
+			{
+				// transform to unsigned notation
+				xtemp = double(temparray[k]);
+				if (temparray[k]<0) xtemp += 256.;
+				// transform back to original signal and apply noise (same as in Granger case)
+				xtemp = xtemp/input_scaling + gsl_ran_gaussian(GSLrandom,std_noise);
+				
+				// xtemp = xtemp/(rawdatabins/input_scaling)*bins;
+				// if (xtemp < 0) xtemp = 0.0;
+				// if (xtemp > bins-1) xtemp = bins-1;
+				
+				// convert to target data format
+				xdata[j][k] = discretize(xtemp, /* 3/4*rawdatabins/input_scaling,*/ 2*std_noise);
+				// xdata[j][k] = rawdata(xtemp);
+			}
+	  }
+	
+		// for(int j=0; j<200; j++)
+		// 	cout <<"t="<<j<<": "<<int(xdata[2][j])<<endl;
+		// exit(1);
+	
+		delete[] temparray;
+	};
+	
+	rawdata discretize(double in)
+	{
+		return discretize(in,0.0);
+	};
+	rawdata discretize(double in, double offset)
+	{
+		return discretize(in,rawdatabins/input_scaling,offset);
+	};
+	rawdata discretize(double in, double max_cap, double offset)
+	{
+		// rescale to target binning
+		double xtemp = in; // *input_scaling*bins/rawdatabins;
+		double xstepsize = (max_cap-0.0)/(bins-2);
+
+		// apply offset to include lower part of Gaussian (noise)
+		xtemp += offset;
+		
+		int xint;
+		if (xtemp > 0.0) xint = floor((xtemp-0.0)/xstepsize) + 1;
+		else xint = 0;
+		
+		// crop overshoot
+		if (xint>=bins) xint = bins-1;
+		if (xint<0) xint = 0;
+		
+		// rawdata result = rawdata(xint);
+		
+		// assert (result < bins);
+		return rawdata(xint);
+	};
+
+	void save_parameters()
+	{
+		char* name = new char[outputfile_pars_name.length()+1];
+		strcpy(name,outputfile_pars_name.c_str());
+		ofstream fileout1(name);
+		delete[] name;
+
+		fileout1.precision(6);		
+		fileout1 <<"{";
+		fileout1 <<"iteration->"<<iteration;
+		
+		fileout1 <<",size->"<<size;
+		fileout1 <<",rawdatabins->"<<rawdatabins;
+		fileout1 <<",bins->"<<bins;
+		fileout1 <<",samples->"<<samples;
+		fileout1 <<",p->"<<word_length;
+		fileout1 <<",noise->"<<std_noise;
+		
+		fileout1 <<"}"<<endl;
+		
+		fileout1.close();
+	};
+
+	void write_result(double **array)
+	{
+		char* name = new char[outputfile_results_name.length()+1];
+		strcpy(name,outputfile_results_name.c_str());
+		ofstream fileout1(name);
+		delete[] name;
+
+		fileout1.precision(6);
+		fileout1 <<fixed;
+	  fileout1 <<"{";
+	  for(int j=0; j<size; j++)
+	  {
+	  	if(j>0) fileout1<<",";
+	  	fileout1 <<"{";
+	    for(unsigned long i=0; i<size; i++)
+	    {
+	      if (i>0) fileout1<<",";
+	    	fileout1 <<(double)array[j][i];
+	    }
+	    fileout1 <<"}"<<endl;
+	  }
+	  fileout1 <<"}"<<endl;
+
+	  cout <<"Transfer entropy matrix saved."<<endl;
+	};
+
+	void write_multidim_result(double ***array)
+	{
+		char* name = new char[outputfile_results_name.length()+1];
+		strcpy(name,outputfile_results_name.c_str());
+		ofstream fileout1(name);
+		delete[] name;
+
+		fileout1.precision(9);
+		fileout1 <<fixed;
+	  fileout1 <<"{";
+	  for(unsigned long j=0; j<size; j++)
+	  {
+	  	if(j>0) fileout1<<",";
+	  	fileout1 <<"{";
+	    for(unsigned long i=0; i<size; i++)
+	    {
+	      if (i>0) fileout1<<",";
+				fileout1 <<"{";
+		    for(int k=0; k<RESULT_DIMENSION; k++)
+				{
+		      if (k>0) fileout1<<",";
+		    	fileout1 <<array[j][i][k];
+				}
+				fileout1 <<"}";
+	    }
+	    fileout1 <<"}"<<endl;
+	  }
+	  fileout1 <<"}"<<endl;
+
+	  cout <<"Transfer entropy matrix saved."<<endl;
+	};
+
+	void generate_global(rawdata** raw, rawdata* global)
+	{
+		double avg;
+		for (unsigned long t=0; t<samples; t++)
+		{
+			avg = 0.0;
+			for (unsigned long j=0; j<size; j++)
+				avg += double(raw[j][t]);
+			global[t] = rawdata(round(avg/size));
+			// test:
+			// global[t] = 3;
+		}
+	};
+};
